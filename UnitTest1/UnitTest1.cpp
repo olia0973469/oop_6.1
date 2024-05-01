06c189d86ff87080add4d0f66f1c434b40a17330
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2024-05-02 02:48:12 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../oop_6.1/Array.h"
+#include "../oop_6.1/Array.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Array arr(5);
+			arr[0] = -16;
+			arr[1] = 2;
+			arr[2] = -35;
+			arr[3] = 43;
+			arr[4] = -47;
+
+			Assert::AreEqual(-16.0, arr[0]);
+			Assert::AreEqual(2.0, arr[1]);
+		}
+	};
+}
---
+++ oop_6.1/FileName.cpp
@@ -0,0 +1,39 @@
+#include "Array.h" 
+#include <iostream> 
+#include <time.h> 
+#include <stdexcept> 
+#include <exception> 
+#include <cmath> 
+
+using namespace std;
+
+typedef Array::value_type* TArray;
+
+int main()
+{
+    int n;
+    cout << "n= "; cin >> n;
+    Array c(n);
+    Array::value_type d[5];
+
+    srand((unsigned)time(NULL));
+    Array::value_type A = -50;
+    Array::value_type B = 50;
+
+    TArray a = new Array::value_type[n];
+
+    for (int i = 0; i < n; i++)
+        a[i] = A + rand() % int(B - A + 1);
+
+
+    Array::iterator l = const_cast<Array::iterator>(c.begin());
+    for (int j = 0; j < 2 * n; j++, l++)
+        *l = a[j];
+    cout << c << endl;
+
+    cout << c.replace(a, n);
+
+    cin.get();
+    cin.get();
+    return 0;
+}
